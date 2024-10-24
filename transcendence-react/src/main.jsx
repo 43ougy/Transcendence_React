97cb56a695b9c2ab3cd78de63ce6619d074dec3b
43ougy <anael.bougy@gmail.com>
2024-10-24 15:36:13 +0200

    Pong Game made

--- transcendence-react/src/main.jsx
+++ transcendence-react/src/main.jsx
@@ -2,10 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import Game from './Game.jsx'
+import Menu from './Menu.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Game/>
+    <Menu/>
   </StrictMode>,
 )
